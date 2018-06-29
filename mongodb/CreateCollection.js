619699fb8631d8c306d3f7a5db9f7668551c9c41
wonseok <wonseok0403@naver.com>
2018-06-30 01:32:44 +0900

    bootstrap study done

--- mongodb/CreateCollection.js
+++ mongodb/CreateCollection.js
@@ -1,4 +1,5 @@
-// npm install mongodb
+// After create Database
+// 2.
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
--- mongodb/CreateDatabase.js
+++ mongodb/CreateDatabase.js
@@ -1,4 +1,5 @@
 // npm install mongodb
+// 1.
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
